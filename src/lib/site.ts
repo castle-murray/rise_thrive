28f421e7750dce04e090ebdf7a014833f1906176
castlemurray <sean@castle-murray.com>
2026-09-22 15:18:13 +0000

    Replace Randall Jones staff photo with new studio headshot.

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -61,7 +61,7 @@ export const staff = [
   {
     name: "Randall Jones",
     role: "Co-Owner / Food Manager · Direct Care Staff",
-    image: "/staff/randall-jones-pvg8b.jpeg",
+    image: "/staff/randall-jones-uk55h.jpeg",
   },
   {
     name: "Bryce Russell",
